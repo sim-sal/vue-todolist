6aac06e823bac9f5ec21579753b557acfa231e0c
sim-sal <simone.saladino93@gmail.com>
2023-05-23 10:11:16 +0200

    added methods to add and remove tasks

--- js/main.js
+++ js/main.js
@@ -4,10 +4,28 @@ createApp({
 
     data(){
         return{
-
+            newTask: '',
+            tasks: []
         }
     },
     methods: {
+        addTask(){
+            if(this.newTask !== ''){
+                const newItem = {
+                    text: this.newTask,
+                    status: false
+                };
+                
+                console.log(newItem);
+                this.tasks.unshift(newItem);
 
+                this.newTask = '';
+            }
+        },
+
+        removeTask(ind){
+            console.log('rimuovi!');
+            this.tasks.splice(ind, 1);
+        }
     }
 }).mount("#app");
